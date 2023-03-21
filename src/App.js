78cc5eeebe23d6f10f273d2f14a9840ffcb65e8f
rabiul155 <siyamict@gmail.com>
2023-03-21 23:20:49 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import { AiOutlineSend } from "react-icons/ai";
+import io from "socket.io-client"
+import Message from './components/Message/Message';
+import { useEffect, useState, } from 'react';
+
+const socket = io.connect('http://localhost:5000')
+
 
 function App() {
+
+  const [mess, setMessage] = useState("")
+  const [messageReceived, setMessageReceived] = useState('')
+  const [components, setComponent] = useState([])
+  const [toggle, setTottle] = useState(true)
+
+  useEffect(() => {
+    socket.on("received_message", (data) => {
+      setMessageReceived(data.message)
+    })
+  }, [])
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    setTottle(!toggle)
+    const m = event.target.message.value;
+    setMessage(m)
+    setComponent([...components, <Message msg={toggle ? m : messageReceived} own={toggle} />]);
+    socket.emit("send_message", { message: m })
+
+
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=' max-w-lg mx-auto pb-4 bg-slate-100 shadow-lg '>
+      <div className='message-box max-w-lg p-4   '>
+
+        {components.map((component, index) => (
+          <div key={index}>{component}</div>
+        ))}
+
+        {/* <Message msg={messageReceived}></Message>
+        <Message msg={mess} own={true}></Message> */}
+
+
+      </div>
+      <div>
+        <form onSubmit={sendMessage} className=''>
+          <div className=" flex px-2">
+            <textarea name='message' placeholder="Write your message" className="p-2 h-[42px] w-full rounded-md focus:ring outline-none focus:ring-opacity-75 focus:ring-violet-400 "></textarea>
+
+            <div className='flex justify-end'>
+              <button type="submit" className="px-3 py-1 font-semibold rounded  bg-blue-800 text-white text-[24px]"><AiOutlineSend className=' inline-flex pb-[4px]'></AiOutlineSend></button>
+            </div>
+          </div>
+        </form>
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Message/Message.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Message = ({ own, msg }) => {
+
+    return (
+        <div>
+            <div className={`flex my-3 ${own ? ' justify-end' : 'flex-col'}`}>
+                <div>
+
+                    {
+                        !own && <p className=' font-[500] text-[13px] relative left-14'>Mikel Bey</p>
+                    }
+
+                    <div className=' flex'>
+                        {
+                            !own &&
+                            <img className=' h-9 w-9 rounded-full mr-3' src="https://i.ibb.co/bH9fpcR/photo-1633332755192-727a05c4013d.jpg" alt="" />
+
+                        }
+
+                        <p className={`px-4 py-1 max-w-[250px] lg:max-w-xs rounded-[18px] ${own ? 'bg-slate-200 text-black' : 'bg-blue-700 text-white'} `}>{msg}</p>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Message;
